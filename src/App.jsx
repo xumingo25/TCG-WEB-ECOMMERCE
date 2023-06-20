47b0ef0d5f3a927d4290c3e982d3daae386d028f
Domingo <domingo.saavedra@tsoftglobal.com>
2023-06-19 21:17:06 -0400

    Se crea v1 del repo con creación de container con MaterialUI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button"
+import Container from "@mui/material/Container"
+
+export default function App() {
+  return (
+    <Container>
+      <h1>App</h1>
+      <Button variant="contained">Mi primer boton</Button>
+    </Container>
+  )
+}
