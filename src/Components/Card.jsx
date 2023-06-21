ea5f26c89551f2376156486a21d24e5eadfb107d
Domingo <domingo.saavedra@tsoftglobal.com>
2023-06-21 13:30:25 -0400

    Se actualiza proyecto. Se mejora la invocación al componente CARD aislandolo de CARDLIST

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -14,69 +14,46 @@ const Img = styled('img')({
   maxHeight: '100%',
 });
 
-const ComplexGrid = () => {
-  const [carta, setCarta] = useState(null);
-
-  useEffect(() => {
-    const random = getRandomInt(6, 1000);
-    fetchData(random);
-  }, []);
-
-  const getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min) + min);
-  };
-
-  const fetchData = async (id) => {
-    try {
-      const response = await fetch(`http://localhost:8080/api/cards/${id}`);
-      const data = await response.json();
-      setCarta(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+const Card = ({ carta }) => {
   return (
-    <Paper
-      sx={{
-        p: 1,
-        margin: 'auto',
-        maxWidth: 225,
-        flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-      }}
-    >
-      <main className="flex">
-        {carta && (
-          <Grid container spacing={2}>
-            <Grid item>
-              <ButtonBase sx={{ height: '100%' }}>
-                <Img alt="complex" src={carta.imagen} className="imgn" />
-              </ButtonBase>
-            </Grid>
-            <Grid item xs={12} sm container>
-              <Grid item xs container direction="column" spacing={2}>
-                <Grid item xs>
-                  <Typography gutterBottom variant="subtitle1" component="div">
-                    <strong>{carta.nombre}</strong>
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid item>
-                <Typography variant="subtitle1" component="div">
-                  <strong>$8000</strong>
+    <Grid item xs={12} sm={6} md={4} lg={3}>
+      <Paper
+        sx={{
+          p: 1,
+          margin: 'auto',
+          maxWidth: 225,
+          flexGrow: 1,
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase sx={{ height: '100%' }}>
+              <Img alt="complex" src={carta.imagen} className="imgn" />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" component="div">
+                  <strong>{carta.nombre}</strong>
                 </Typography>
               </Grid>
-              <Button variant="contained" endIcon={<AddShoppingCartSharpIcon />}>
-                Agregar al carrito
-              </Button>
             </Grid>
+            <Grid item>
+              <Typography variant="subtitle1" component="div">
+                <strong>$8.000</strong>
+              </Typography>
+            </Grid>
+            <Button variant="contained" endIcon={<AddShoppingCartSharpIcon />}>
+              Agregar al carrito
+            </Button>
           </Grid>
-        )}
-      </main>
-    </Paper>
+        </Grid>
+      </Paper>
+    </Grid>
   );
 };
 
-export default ComplexGrid;
+export default Card;
--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -9,55 +9,7 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-
-const Img = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-});
-
-const Card = ({ carta }) => {
-  return (
-    <Grid item xs={12} sm={6} md={4} lg={3}>
-      <Paper
-        sx={{
-          p: 1,
-          margin: 'auto',
-          maxWidth: 225,
-          flexGrow: 1,
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase sx={{ height: '100%' }}>
-              <Img alt="complex" src={carta.imagen} className="imgn" />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1" component="div">
-                  <strong>{carta.nombre}</strong>
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1" component="div">
-                <strong>$8.000</strong>
-              </Typography>
-            </Grid>
-            <Button variant="contained" endIcon={<AddShoppingCartSharpIcon />}>
-              Agregar al carrito
-            </Button>
-          </Grid>
-        </Grid>
-      </Paper>
-    </Grid>
-  );
-};
+import Card from './Card';
 
 const CardList = () => {
   const [cartas, setCartas] = useState([]);
